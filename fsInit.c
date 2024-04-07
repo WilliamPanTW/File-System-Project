8b0e3ccdaf24f890ed8dc4603ac9014d07779206
student <william910122@gmail.com>
2024-04-07 10:15:44 -0700

    fsInit.h include

--- fsInit.c
+++ fsInit.c
@@ -20,35 +20,53 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "fsLow.h"
 #include "mfs.h"
+#include "fsInit.h"
+
+char * freeSpace; //global bitmap
 
 
 int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	{
 	printf ("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
-	
-	typedef struct vcb{
-    uint64_t block_size;		//Total Number of blocks
-	uint64_t block_index;		//number of free blocks in volume
-    
-	uint64_t free_block_size;	//Total number of free blocks(bitmap length)
-	uint64_t free_block_index;	//location of the free space in bitmap
-	
-	uint64_t root_dir_size;		//Total number  of the root directory 
-	uint64_t root_dir_index;	//Location of the root directory 
-	
-	uint64_t signature;			//Long type unique identify (8bytes) generate by magic number 
-	} vcb;
 
-	char * freespace;
+
+
+
+	//Malloc a Block of memory as your VCB pointer and LBAread block 0
+	freeSpace = malloc(vcb.block_size * BLOCK_SIZE);
+
+	//initialize value in vcb 
+	vcb.signature = PART_SIGNATURE;
+	vcb.block_size = numberOfBlocks;
+	// LBAwrite the VCB to block 0
+	LBAwrite(&vcb, 1, 0);
+
+
+
+	void set_bit(char* bitmap, int position);
+	int get_bit(char* bitmap, int position);
+	void clear_bit(char* bitmap, int position);
+
+
+	int bytesNeeded = numberOfBlocks / 8; //8 bits(1 bytes) 8 blocks 
+
 	return 0;
 	}
 	
 	
 void exitFileSystem ()
 	{
+
+	//Free bitmap
+    free(freeSpace);
+    freeSpace = NULL;
+
+
 	printf ("System exiting\n");
 	}
+
---
+++ fsInit.h
@@ -0,0 +1,54 @@
+/**************************************************************
+* Class::  CSC-415-01 Spring 2024
+* Name:: Pan William
+* Student IDs:: 922867228
+* GitHub-Name:: WilliamPanTW
+* Group-Name:: JMWT
+* Project:: Basic File System
+*
+* File:: fsInit.h
+*
+* Description:: 
+*
+**************************************************************/
+#ifndef FSINIT
+#define FSINIT
+
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#define MAX_FILENAME_LENGTH 255
+#define BLOCK_SIZE 512
+
+
+struct dirEntry 
+	{
+    char filename[MAX_FILENAME_LENGTH];// char name size with null-terminator
+
+    uint64_t location;               // long type of address(location)
+
+    uint32_t isDirectory;            // indicating it's a directory(1) or a file(0)
+    uint32_t dirSize;               // unsigned integer of directory size
+
+    time_t createDate;           // integer or long depend on implementation
+    time_t modifyDate;           // integer or long depend on implementation
+    time_t accessDate;          // integer or long depend on implementation
+	} dirEntry ;
+
+
+struct vcb
+	{
+    uint64_t block_size;		//Total Number of blocks
+	uint64_t block_index;		//number of free blocks in volume
+    
+	uint64_t free_block_size;	//Total number of free blocks(bitmap length)
+	uint64_t free_block_index;	//location of the free space in bitmap
+	
+	uint64_t root_dir_size;		//Total number  of the root directory 
+	uint64_t root_dir_index;	//Location of the root directory 
+	
+	uint64_t signature;			//Long type unique identify (8bytes) generate by magic number 
+	} vcb;
+
+#endif
+
